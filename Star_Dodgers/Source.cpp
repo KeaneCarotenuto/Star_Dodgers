afe988b6d2ea2da88b1b5324a3a6db122f6bbf34
Keane <KeaneCarotenuto@gmail.com>
2021-08-09 16:36:52 +1200

    Particle Test [TO BE REMOVED]

--- Star_Dodgers/Source.cpp
+++ Star_Dodgers/Source.cpp
@@ -3,14 +3,97 @@
 #include <Windows.h>
 #include <iostream>
 
+class ParticleSystem : public sf::Drawable, public sf::Transformable
+{
+public:
+
+	ParticleSystem(unsigned int count) :
+		m_particles(count),
+		m_vertices(sf::Lines, count),
+		m_lifetime(sf::seconds(3.f)),
+		m_emitter(0.f, 0.f)
+	{
+	}
+
+	void setEmitter(sf::Vector2f position)
+	{
+		m_emitter = position;
+	}
+
+	void update(sf::Time elapsed)
+	{
+		for (std::size_t i = 0; i < m_particles.size(); ++i)
+		{
+			// update the particle lifetime
+			Particle& p = m_particles[i];
+			p.lifetime -= elapsed;
+
+			// if the particle is dead, respawn it
+			if (p.lifetime <= sf::Time::Zero)
+				resetParticle(i);
+
+			// update the position of the corresponding vertex
+			m_vertices[i].position += p.velocity * elapsed.asSeconds();
+
+			// update the alpha (transparency) of the particle according to its lifetime
+			float ratio = p.lifetime.asSeconds() / m_lifetime.asSeconds();
+			m_vertices[i].color.a = static_cast<sf::Uint8>(ratio * 255);
+		}
+	}
+
+private:
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+	{
+		// apply the transform
+		states.transform *= getTransform();
+
+		// our particles don't use a texture
+		states.texture = NULL;
+
+		// draw the vertex array
+		target.draw(m_vertices, states);
+	}
+
+private:
+
+	struct Particle
+	{
+		sf::Vector2f velocity;
+		sf::Time lifetime;
+	};
+
+	void resetParticle(std::size_t index)
+	{
+		// give a random velocity and lifetime to the particle
+		float angle = (std::rand() % 360) * 3.14f / 180.f;
+		float speed = (std::rand() % 50) + 50.f;
+		m_particles[index].velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
+		m_particles[index].lifetime = sf::milliseconds((std::rand() % 2000) + 1000);
+
+		// reset the position of the corresponding vertex
+		m_vertices[index].position = m_emitter;
+	}
+
+	std::vector<Particle> m_particles;
+	sf::VertexArray m_vertices;
+	sf::Time m_lifetime;
+	sf::Vector2f m_emitter;
+};
+
 int main() {
 
 	//Creating Different Windows
-	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Star Dodgers - By TEAMNAME", sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Star Dodgers - By ClosedGL", sf::Style::Default);
 
 	sf::Image icon;
 	icon.loadFromFile("Resources/icon.png");
 	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+
+	// create the particle system
+	ParticleSystem particles(10000);
+
+	sf::Clock clock;
 
 	while (window.isOpen() == true)
 	{
@@ -24,6 +107,14 @@ int main() {
 				window.close();
 			}
 		}
+
+		sf::Time elapsed = clock.restart();
+		particles.update(elapsed);
+
+		// draw it
+		window.clear();
+		window.draw(particles);
+		window.display();
 	}
 
 	return 0;
