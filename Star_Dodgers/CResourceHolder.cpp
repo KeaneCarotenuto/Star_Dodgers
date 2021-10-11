18087db5df830ade7aa90b79c09f3acb7811d568
Nerys-Thamm <nerysthamm@mgail.com>
2021-10-11 19:10:48 +1300

    Capped FPS at 60

--- Star_Dodgers/CResourceHolder.cpp
+++ Star_Dodgers/CResourceHolder.cpp
@@ -19,6 +19,7 @@ void CResourceHolder::Initialise()
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
 
 	m_window = new sf::RenderWindow(sf::VideoMode(1920, 1080 - titlebarHeight - taskbarBeight, desktop.bitsPerPixel), "Star Dodgers - By ClosedGL", sf::Style::Default);
+	m_window->setFramerateLimit(60);
 	m_window->setPosition({ 0, 0 });
 	m_window->requestFocus();
 
