54076982ac498fee63761ac556d7ce955199ad6f
Keane <KeaneCarotenuto@gmail.com>
2021-11-12 17:35:04 +1300

    Not always bullet hell

--- Star_Dodgers/CBall.cpp
+++ Star_Dodgers/CBall.cpp
@@ -614,8 +614,8 @@ void CBall::ForceCatch(CPlayer* _player)
 		SetWinningBall();
 	}
 	else {
-		//m_power = static_cast<BallPower>(1 + (rand() % 4));
-		m_power = BallPower::BulletHell;
+		m_power = static_cast<BallPower>(1 + (rand() % 4));
+		//m_power = BallPower::BulletHell;
 		m_powerupSFX.play();
 	}
 
